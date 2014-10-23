5c222c10a059a89e327366e9ad5eb5beb2dd1f9b
enricomarino <enrico.marino@email.com>
2014-10-23 18:58:44 +0200

    added strongloop and polymer

---
+++ client/index.js
@@ -0,0 +1,26 @@
+var app = document.getElementById('app');
+
+var open = function (page_name, details) {
+  return function (context) {  
+    var element = document.createElement(page_name);
+    context.element = element;
+    while (app.firstChild) app.removeChild(app.firstChild);
+    app.appendChild(element);
+    setTimeout(function () {
+      element.details = details;    
+    }, 10);
+  };
+};
+
+var start = function () {
+  page('/', open('page-home'));
+  page('/policy', open('page-policy'));
+  page('*', open('page-home'));
+  page();
+
+  page.show(location.pathname);
+
+  console.log('Welcome to Codedoc Academy!');
+};
+
+start();
