2ef8a6c3bbb900076dcf2f3fb60bb765d4464270
Federico Spini <federico.spini@gmail.com>
2014-10-30 19:15:32 +0100

    fixed catch all route on server

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,12 +1,7 @@
-var path = require('path');
 module.exports = function(server) {
   // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-
+  // var router = server.loopback.Router();
   // router.get('/', server.loopback.status());
-  
-  router.get('/', function (req, res) { res.sendfile(server.index_file_path); });
-  
-  server.use(router);
+  // server.use(router);
   server.dataSources.email.connector.transports[0].transporter.options.auth.pass = process.env.GMAIL_PASS;
 };
