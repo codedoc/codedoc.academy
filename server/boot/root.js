cfa98c206827454e43228c451477267e899b2758
enricomarino <enrico.marino@email.com>
2014-10-30 18:52:44 +0100

    crumbs

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,11 +1,11 @@
 var path = require('path');
 module.exports = function(server) {
   // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  // router.get('/', server.loopback.status());
-  router.get('/', function (req, res) {
-    res.sendfile(server.index_file_path);
-  });
-  server.use(router);
+  // var router = server.loopback.Router();
+  // // router.get('/', server.loopback.status());
+  // router.get('/', function (req, res) {
+  //   res.sendfile(server.index_file_path);
+  // });
+  // server.use(router);
   server.dataSources.email.connector.transports[0].transporter.options.auth.pass = process.env.GMAIL_PASS;
 };
--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.index_file_path = path.resolve(__dirname, '../client/index.html');
 // passing the static middleware are hitting the file system
 app.use(loopback.static(path.dirname(app.index_file_path)));
 
+// var router = app.loopback.Router();
+// router.get('/', server.loopback.status());
+app.get('*', function (req, res) {
+  res.sendFile(app.index_file_path);
+});
+// app.use(router);
+
+
 // Requests that get this far won't be handled
 // by any middleware. Convert them into a 404 error
 // that will be handled later down the chain.
