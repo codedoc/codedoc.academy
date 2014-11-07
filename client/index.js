5684e6c4e969e461df6bdc02fb2da45ebde3b988
Federico Spini <federico.spini@gmail.com>
2014-11-07 12:47:18 +0100

    doublechecked router

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
-// // google analytics
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-// ga('create', 'UA-56515755-1', 'auto');
+// google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-56515755-1', 'auto');
 
 
 // app boot
@@ -12,7 +12,7 @@ const host_re = /^[www\.]?hackfaber\.com/;
 var hostname = window.location.hostname;
 var host_query = host_re.exec(hostname);
 var env = host_query && host_query[0] === 'hackfaber.com' ? 'production' : 'develop';
-var production = function () { return env === 'production'; };
+var production = function () { /* return env === 'production'; */ return true;};
 
 var app = document.getElementById('app');
 
